d5bee3e5805fab692e12e0dd6690ad6e6f58502d
Abhishek SS <abhishekvinu682@gmail.com>
2025-09-17 21:11:11 +0530

    commit

--- ecom_server/src/main/java/com/bootA/ecom_server/controllers/ProductsController.java
+++ ecom_server/src/main/java/com/bootA/ecom_server/controllers/ProductsController.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.bootA.ecom_server.exception.ResorceNotFoundException;
+// import com.bootA.ecom_server.exception.ResorceNotFoundException;
 import com.bootA.ecom_server.models.Product;
 import com.bootA.ecom_server.repos.ProductRepo;
 
